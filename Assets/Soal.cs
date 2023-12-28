65b848d216dfabe264d39be7a3ea9d4ec9ea52c5
Riyana-wulAN <riyanawulannurhidayah@gmail.com>
2023-12-28 15:02:13 +0700

    mintol ji

---
+++ Assets/Soal.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soal : MonoBehaviour
+{
+    public void openChanel()
+    {
+        Application.OpenURL("https://wordwall.net/resource/59350303");
+    }
+}
